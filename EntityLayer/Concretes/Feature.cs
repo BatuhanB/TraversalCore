e91ebc2fb60cb3a950cabd6003e064f093d7dffb
BatuhanB <balibatuhan19@gmail.com>
2022-04-01 12:22:05 +0300

    project has been uploaded

---
+++ EntityLayer/Concretes/Feature.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concretes
+{
+    public class Feature
+    {
+        [Key]
+        public int Feature1ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ EntityLayer/Concretes/NewsLatter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concretes
+{
+    public class NewsLatter
+    {
+        public int NewsletterID { get; set; }
+        public string Mail { get; set; }
+    }
+}
